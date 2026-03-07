c396038df3a296dd5448d356c6f75ed3b255513d
JDANIBLACK7 <jdespejo2004@gmail.com>
2026-03-07 13:47:13 -0400

    javascript

---
+++ Javascript/assets/javascript/parametros-funciones.js
@@ -0,0 +1,22 @@
+let n1 = 15;
+let n2 = 30;
+let n3 = '45';
+
+//console.log(parseInt(n1)); //esto es una funcion porque coloco el nombre de la funcion
+//acompañada de ()= nombreFunction() antes de la variable;
+//console.log(n2.toString()); //esto es un metodo va despues de una variable
+//console.log(parseInt(n3)); //este metodo me convierte en numero string
+
+//Incrementadores en JavaScript
+var b = 15;
+var c = '15';
+//console.log(b++);
+//console.log(b++);
+//console.log(++b);
+//console.log(--b);
+console.log(--b);
+
+var compruebo = b !== c; //valido que sean distintos tanto en valor como en tipo de datos
+var compruebo2 = b === c; //comprobar que dos variables tengan tanto el mismo valor como que sean del mismo valor como que sean del mismo tipo de datos
+console.log(compruebo);
+console.log(compruebo2);
